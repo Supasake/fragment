be81479c06d7c7d941ca70608551ee8ad66d91d2
Tae <palasri.sp@gmail.com>
2016-11-18 09:37:40 +0700

    Start App

---
+++ app/src/main/java/com/example/tae/myfragment/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tae.myfragment;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity implements BlankFragment.OnFragmentInteractionListener{
+
+    TextView myText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
